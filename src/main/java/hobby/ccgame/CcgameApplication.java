f3cd7124aa660499381d66ce78e3964b8a84e952
Dudik Pál <dudikpal@gmail.com>
2022-03-09 09:52:57 +0100

    Init commit

---
+++ src/main/java/hobby/ccgame/CcgameApplication.java
@@ -0,0 +1,13 @@
+package hobby.ccgame;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CcgameApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CcgameApplication.class, args);
+    }
+
+}
